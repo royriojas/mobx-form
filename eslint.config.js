c952a12cf5860a08f4dccb4169a8ff8a31f951cf
Roy Riojas <royriojas@gmail.com>
2026-02-14 06:27:15 -0800

    FEAT: Add tests using action

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,9 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
 
 import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import tailwind from "eslint-plugin-tailwindcss";
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config({
@@ -20,21 +17,14 @@ export default tseslint.config({
   plugins: {
     'react-hooks': reactHooks,
     'react-refresh': reactRefresh,
-    tailwindcss: tailwind,
     prettier: eslintPluginPrettierRecommended.plugins.prettier,
   },
   rules: {
     ...reactHooks.configs.recommended.rules,
-    ...tailwind.configs.recommended.rules,
     ...eslintPluginPrettierRecommended.rules,
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-  settings: {
-    tailwindcss: {
-      calee: ["classnames", "clsx", "ctl", "cn", "cnByMatchesH", "twmerge", "tw", "twMerge"],
-    }
-  }
-}, storybook.configs["flat/recommended"]);
+});
