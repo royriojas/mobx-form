85a60bb1bf86068c7005f578edc3712b451dbe94
Roy Riojas <royriojas@gmail.com>
2026-02-26 01:56:54 -0800

    FIX: fix path to types

--- bundle-types.sh
+++ bundle-types.sh
@@ -4,5 +4,13 @@ bunx dts-bundle \
 --name mobx-form \
 --baseDir dist \
 --main dist/index.d.ts \
---out dist/mobx-form.d.ts \
+--out mobx-form.d.ts \
 --removeSource
+
+# verify existance of mobx-form.d.ts
+if [ -f "dist/mobx-form.d.ts" ]; then
+  echo "mobx-form.d.ts exists"
+else
+  echo "mobx-form.d.ts does not exist"
+  exit 1
+fi
